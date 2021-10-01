4ffe01291c96d04bbe5e5653d7ded61aaa47383e
Ali Zaryab Naeem <58973388+aleezaryabnaeem@users.noreply.github.com>
2021-10-01 14:52:35 +0500

    Menu Updated

--- src/pages/App.js
+++ src/pages/App.js
@@ -239,7 +239,7 @@ export default function App() {
                                 </a>
                             </li>*/}
                         </ul>
-                        <div className="dropdown ForCollapseView text-center">
+                        {/*<div className="dropdown ForCollapseView text-center">
                             <a href="#" id="SettingOptions" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#888888"
                                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -293,7 +293,7 @@ export default function App() {
                                     <li><a className="dropdown-item px-2" href="#">Dutch</a></li>
                                 </ul>
                             </div>
-                        </div>
+                        </div>*/}
                     </div>
                     <ContentArea id="ContentArea">
                         <Popups/>
